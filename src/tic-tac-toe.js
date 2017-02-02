2bf87ac36073bdb767e367935fc05199c25a63e8
Dmitry Drozd <drozd.dzmitryi@gmail.com>
2017-02-02 23:53:45 +0300

    added noMoreTurns, isDraw, getFieldValue, GetCurrentPlayerSymbol and constructor logycs

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,14 +1,18 @@
 class TicTacToe {
     constructor() {
-
+        this.currentPlayer = 'x';
+        this.desk = [[],[],[]];
+        this.dimension = 3;
     }
 
     getCurrentPlayerSymbol() {
-
+        return this.currentPlayer;
     }
 
     nextTurn(rowIndex, columnIndex) {
+        this.desk[rowIndex][columnIndex] = this.currentPlayer;
 
+        this.currentPlayer = this.currentPlayer == 'x'? 'o' : 'x';
     }
 
     isFinished() {
@@ -20,15 +24,20 @@ class TicTacToe {
     }
 
     noMoreTurns() {
+        for (let i = 0; i < this.dimension; i++)
+            for (let j = 0; j < this.dimension; j++)
+                if (this.desk[i][j] == undefined)
+                    return false;
 
+        return true;
     }
 
     isDraw() {
-
+        return this.noMoreTurns() && !this.getWinner();
     }
 
     getFieldValue(rowIndex, colIndex) {
-
+        return this.desk[rowIndex][colIndex];
     }
 }
 
